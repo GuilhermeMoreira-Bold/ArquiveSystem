d45f049826e20c3164d44912fcbb6f8d18b13feb
Guilherme <gulhermejacinto1@gmail.com>
2025-02-12 23:25:06 -0300

    Documentation_2

--- src/main/java/org/example/system/directories/Directory.java
+++ src/main/java/org/example/system/directories/Directory.java
@@ -8,39 +8,44 @@ import java.util.Map;
 import org.example.system.arquives.Arquive;
 
 /**
- * Representa um diretório no sistema de arquivos.
- * Um diretório pode conter arquivos e subdiretórios.
+ * Representa um diretório dentro de um sistema de arquivos.
+ * Um diretório pode conter arquivos e subdiretórios,
+ * e possui informações sobre seu estado e bloco inicial.
  */
 public class Directory {
-
-    /** Nome do diretório. */
     String name;
-
-    /** Lista de arquivos armazenados no diretório. */
     List<Arquive> data;
-
-    /** Map de subdiretórios (filhos) do diretório. */
     Map<String, Directory> childrens;
-
-    /** Diretório pai do diretório atual. */
     Directory parent;
-
-    /** Status do diretório. Pode indicar se o diretório está ocupado ou livre, por exemplo. */
     byte status;
-
-    /** Bloco inicial onde o diretório está localizado. */
     int staterBlock;
-
-    /** Tamanho do diretório, representado pela quantidade de arquivos e subdiretórios. */
     int size = 0;
 
     /**
-     * Constrói um diretório com nome, diretório pai, status e bloco inicial.
+     * Define o bloco inicial do diretório.
      *
-     * @param name Nome do diretório.
-     * @param parent Diretório pai do diretório atual.
-     * @param status Status do diretório (ocupado, livre, etc.).
-     * @param staterBlock Bloco inicial do diretório.
+     * @param staterBlock o bloco inicial do diretório.
+     */
+    public void setStaterBlock(int staterBlock) {
+        this.staterBlock = staterBlock;
+    }
+
+    /**
+     * Retorna o mapa de subdiretórios (filhos) do diretório.
+     *
+     * @return mapa contendo os subdiretórios do diretório.
+     */
+    public Map<String, Directory> getChildrens() {
+        return childrens;
+    }
+
+    /**
+     * Construtor que cria um diretório com o nome, diretório pai, status e bloco inicial.
+     *
+     * @param name o nome do diretório.
+     * @param parent o diretório pai.
+     * @param status o status do diretório.
+     * @param staterBlock o bloco inicial do diretório.
      */
     public Directory(String name, Directory parent, byte status, int staterBlock) {
         this.name = name;
@@ -52,12 +57,12 @@ public class Directory {
     }
 
     /**
-     * Constrói um diretório com nome, diretório pai e status.
-     * O bloco inicial será determinado mais tarde.
+     * Construtor que cria um diretório com o nome, diretório pai e status.
+     * O bloco inicial será atribuído automaticamente.
      *
-     * @param name Nome do diretório.
-     * @param parent Diretório pai do diretório atual.
-     * @param status Status do diretório (ocupado, livre, etc.).
+     * @param name o nome do diretório.
+     * @param parent o diretório pai.
+     * @param status o status do diretório.
      */
     public Directory(String name, Directory parent, byte status) {
         this.name = name;
@@ -68,86 +73,86 @@ public class Directory {
     }
 
     /**
-     * Retorna o número de arquivos e subdiretórios dentro do diretório.
+     * Retorna o número total de arquivos e subdiretórios contidos no diretório.
      *
-     * @return O tamanho do diretório, que é a quantidade de arquivos e subdiretórios.
+     * @return o tamanho do diretório.
      */
     public int getSize() {
         return size;
     }
 
     /**
-     * Retorna o bloco inicial onde o diretório está localizado.
+     * Retorna o bloco inicial do diretório.
      *
-     * @return O bloco inicial do diretório.
+     * @return o bloco inicial do diretório.
      */
     public int getStaterBlock() {
         return staterBlock;
     }
 
     /**
-     * Retorna o status do diretório (por exemplo, se está ocupado ou livre).
+     * Retorna o status do diretório.
      *
-     * @return O status do diretório.
+     * @return o status do diretório.
      */
     public byte getStatus() {
         return status;
     }
 
     /**
-     * Adiciona um arquivo ao diretório.
+     * Adiciona um arquivo ao diretório e aumenta o tamanho do diretório.
      *
-     * @param arquive O arquivo a ser adicionado.
+     * @param arquive o arquivo a ser adicionado.
      */
     public void addData(Arquive arquive) {
-        size++; // Incrementa o tamanho do diretório
-        data.add(arquive); // Adiciona o arquivo à lista de dados
+        size++;
+        data.add(arquive);
     }
 
     /**
-     * Remove um arquivo do diretório.
+     * Remove um arquivo do diretório e diminui o tamanho do diretório.
      *
-     * @param arquive O arquivo a ser removido.
+     * @param arquive o arquivo a ser removido.
      */
     public void removeData(Arquive arquive) {
-        size--; // Decrementa o tamanho do diretório
-        data.remove(arquive); // Remove o arquivo da lista de dados
+        size--;
+        data.remove(arquive);
     }
 
     /**
-     * Adiciona um subdiretório ao diretório.
+     * Adiciona um subdiretório ao diretório e aumenta o tamanho do diretório.
      *
-     * @param name O nome do subdiretório.
-     * @param subdirectory O subdiretório a ser adicionado.
+     * @param name o nome do subdiretório.
+     * @param subdirectory o subdiretório a ser adicionado.
      */
     public void addSubdirectory(String name, Directory subdirectory) {
-        size++; // Incrementa o tamanho do diretório
-        childrens.put(name, subdirectory); // Adiciona o subdiretório ao mapa de subdiretórios
+        size++;
+        childrens.put(name, subdirectory);
     }
 
     /**
-     * Remove um subdiretório do diretório.
+     * Remove um subdiretório do diretório e diminui o tamanho do diretório.
      *
-     * @param subdirectory O subdiretório a ser removido.
+     * @param subdirectory o subdiretório a ser removido.
      */
     public void removeSubdirectory(Directory subdirectory) {
-        size--; // Decrementa o tamanho do diretório
-        childrens.remove(subdirectory.name); // Remove o subdiretório do mapa de subdiretórios
+        size--;
+        childrens.remove(subdirectory.name);
     }
 
     /**
      * Retorna o nome do diretório.
      *
-     * @return O nome do diretório.
+     * @return o nome do diretório.
      */
     public String getName() {
         return name;
     }
 
     /**
-     * Retorna a lista de arquivos armazenados no diretório.
+     * Retorna a lista de arquivos contidos no diretório.
      *
-     * @return A lista de arquivos do diretório.
+     * @return a lista de arquivos do diretório.
      */
     public List<Arquive> getData() {
         return data;
@@ -156,21 +161,20 @@ public class Directory {
     /**
      * Retorna o diretório pai do diretório atual.
      *
-     * @return O diretório pai.
+     * @return o diretório pai.
      */
     public Directory getParent() {
         return parent;
     }
 
     /**
-     * Representa o diretório como uma string.
-     * Inclui informações sobre o nome, arquivos, subdiretórios e status.
+     * Retorna uma representação em string do diretório, incluindo seus dados, filhos e outras propriedades.
      *
-     * @return A representação em string do diretório.
+     * @return a representação em string do diretório.
      */
     @Override
     public String toString() {
-        if (parent != null) { // Se o diretório tem um diretório pai, exibe o nome do pai
+        if (parent != null) {
             return "Directory{" +
                     "name='" + name + '\'' +
                     ", data=" + data +
@@ -180,7 +184,6 @@ public class Directory {
                     ", starter_block=" + staterBlock +
                     '}';
         }
-        // Caso contrário, exibe o diretório sem o diretório pai
         return "Directory{" +
                 "name='" + name + '\'' +
                 ", data=" + data +
@@ -191,20 +194,18 @@ public class Directory {
     }
 
     /**
-     * Retorna o caminho completo do diretório a partir da raiz.
-     * O caminho é construído adicionando os diretórios pais.
+     * Retorna o caminho completo do diretório, começando da raiz.
      *
-     * @return O caminho completo do diretório.
+     * @return o caminho completo do diretório.
      */
     public String getPath() {
         StringBuilder path = new StringBuilder();
         Directory d = this;
 
-        // Subindo na árvore de diretórios até a raiz
-        while (true) {
-            if (d == null) break; // Se não houver mais diretórios pais, para de subir
-            path.insert(0, d.getName().equals("/") ? "" : "/" + d.getName()); // Adiciona o nome do diretório ao caminho
-            d = d.getParent(); // Vai para o diretório pai
+        while(true){
+            if(d == null) break;
+            path.insert(0, d.getName().equals("/") ? "" : "/" + d.getName());
+            d = d.getParent();
         }
 
         return path.toString();
